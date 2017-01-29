c9a0f0cc8a1e1afff0c1f343ddb797f55bef2608
Apollo Tang <tang.apollo@gmail.com>
2017-01-28 21:36:57 -0500

    app/ is now the root for applicaiotn

--- src/app/index.js
+++ src/app/index.js
@@ -1,3 +1,7 @@
+if (process && process.env && process.env.CONSOLE_LOG) {
+    console.info('log from file: app/index.js'); // eslint-disable-line no-console
+}
+
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
@@ -8,9 +12,10 @@ import FilteredTodos from 'app/containers/filtered-todos';
 
 const store = createStore(rootReducer);
 
+import style from './style';
 const App = () => (
     <Provider store={store}>
-        <div className="layout" >
+        <div className={`app ${style['module-style']}`}>
             <FilteredTodos />
         </div>
     </Provider>
