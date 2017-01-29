6441be2c6983c06d6aa7abe0ac657d3aa08e24f4
Apollo Tang <tang.apollo@gmail.com>
2017-01-29 01:42:55 -0500

    link show as text if active

--- src/app/containers/todo-list-filter-setting-ctrl/selector.js
+++ src/app/containers/todo-list-filter-setting-ctrl/selector.js
@@ -3,7 +3,7 @@ import  * as Action from 'app/actions/todo-list-control';
 
 const mapStoreToProps = (store, ownProps) => {
     return {
-        active: ownProps.filterType === store.todoListFilter
+        active: ownProps.filterType === store.todoListControl.filter
     }
 };
 
